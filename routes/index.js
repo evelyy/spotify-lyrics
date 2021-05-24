00fe00eb94f77cb92fc5b58a46f89591ceaa7aee
evelyy <evelyn.k.heaton@gmail.com>
2021-05-24 22:58:59 +1000

    fixed webpage

--- routes/index.js
+++ routes/index.js
@@ -10,21 +10,19 @@ router.use(cookieParser());
 // GET home page
 router.get('/', (req, res, next) => {
     if(req.cookies.accessToken) {
-        console.log(req.cookies.accessToken);
         superagent
             .get('https://api.spotify.com/v1/me/player')
             .set('Authorization', `Bearer ${req.cookies.accessToken}`)
             .set('Content-Type', 'application/json')
             .set('Accept', 'application/json')
             .then(spotifyRes => {
-                const track = JSON.parse(spotifyRes.text).item
-                res.render('playing', { trackName: track.name, artistName: track.artists[0].name});
+                const track = JSON.parse(spotifyRes.text).item;
+                res.render('playing', { title: `now playing: ${track.name}`, trackName: track.name, artistName: track.artists[0].name, albumArt: track.album.images[1].url });
             })
             .catch(spotifyErr => {
                 res.render('error');
                 next(createError(spotifyErr));
             })
-
     } else {
         res.redirect('/login');
     }
