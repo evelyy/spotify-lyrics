15c7bfdaa8f20535714cbc9c6a5fc10d761e2f07
evelyy <evelyn.k.heaton@gmail.com>
2021-05-24 22:32:50 +1000

    spotify api interfacing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+const superagent = require('superagent');
+
+
+router.use(cookieParser());
 
 // GET home page
 router.get('/', (req, res, next) => {
-    res.render('index', { title: 'Express' });
+    if(req.cookies.accessToken) {
+        console.log(req.cookies.accessToken);
+        superagent
+            .get('https://api.spotify.com/v1/me/player')
+            .set('Authorization', `Bearer ${req.cookies.accessToken}`)
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .then(spotifyRes => {
+                const track = JSON.parse(spotifyRes.text).item
+                res.render('playing', { trackName: track.name, artistName: track.artists[0].name});
+            })
+            .catch(spotifyErr => {
+                res.render('error');
+                next(createError(spotifyErr));
+            })
+
+    } else {
+        res.redirect('/login');
+    }
 });
 
 module.exports = router;
